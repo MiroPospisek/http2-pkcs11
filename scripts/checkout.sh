c4dfc0dc7aa6a1225845582162d6e1bc5b15fc09
Daniel Kubec <niel@rtfm.cz>
2017-06-10 22:14:11 +0200

    Added support of http2 jetty

--- scripts/checkout.sh
+++ scripts/checkout.sh
@@ -19,4 +19,5 @@ git clone https://github.com/openssl/openssl.git
 git clone https://github.com/OpenSC/libp11.git
 git clone https://github.com/nghttp2/nghttp2.git
 git clone https://github.com/apache/tomcat-native.git
+git clone https://github.com/netty/netty-tcnative.git
 cd ..
